fc7b2a80bcc107116a3471cd9c5cabf7cb064f87
fweidner <info@florianweidner.de>
2017-05-28 11:00:33 +0200

    Moved .git folder.

--- Source/Plugin_Socket_cpp/Plugin_Socket_cpp.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Plugin_Socket_cpp.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Plugin_Socket_cpp, "Plugin_Socket_cpp" );
--- Source/Plugin_Socket_cpp/Plugin_Socket_cppGameModeBase.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Plugin_Socket_cpp.h"
-#include "Plugin_Socket_cppGameModeBase.h"
-
-
-
-
